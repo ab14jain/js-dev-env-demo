3b7808c76c78516108b899e36f4418846c45ff65
unknown <ab14jain@gmail.com>
2018-06-16 12:59:12 +0530

    updated package.json

---
+++ buildScripts/distServer.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import compression from 'compression';
+var path =  require('path');
+var open = require('open');
+const port = 3333;
+const app = express();
+
+app.use(compression());
+app.use(express.static('dist'));
+
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
+
+app.listen(port, function(err){
+    if(err){
+      console.log("err");
+    }
+    else{
+      open('http://localhost:' + port);
+    }
+});
